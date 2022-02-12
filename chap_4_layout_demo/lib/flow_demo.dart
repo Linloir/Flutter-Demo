5ac2db240a9fd1338f3af97046bbfc5cfb918f7a
Linloir <3145078758@qq.com>
2022-02-13 00:28:42 +0800

    Update Flow demo

--- chap_4_layout_demo/lib/flow_demo.dart
+++ chap_4_layout_demo/lib/flow_demo.dart
@@ -1,9 +1,11 @@
 /*
  * @Author       : Linloir
  * @Date         : 2022-02-10 14:50:47
- * @LastEditTime : 2022-02-10 20:59:57
+ * @LastEditTime : 2022-02-13 00:11:33
  * @Description  : Flow layout demo page
  */
+
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 
@@ -19,7 +21,18 @@ class FlowPage extends StatelessWidget {
       body: Flow(
         delegate: MyFlowDelegate(margin: const EdgeInsets.fromLTRB(5, 2, 5, 2)),
         children: [
-
+          Container(width: 80.0, height: 80.0, color: Colors.red,),
+          Container(width: 80.0, height: 80.0, color: Colors.blue,),
+          Container(width: 80.0, height: 80.0, color: Colors.green,),
+          Container(width: 80.0, height: 80.0, color: Colors.purple,),
+          Container(width: 80.0, height: 80.0, color: Colors.teal,),
+          Container(width: 80.0, height: 80.0, color: Colors.pink,),
+          Container(width: 80.0, height: 80.0, color: Colors.red,),
+          Container(width: 80.0, height: 80.0, color: Colors.blue,),
+          Container(width: 80.0, height: 80.0, color: Colors.green,),
+          Container(width: 80.0, height: 80.0, color: Colors.purple,),
+          Container(width: 80.0, height: 80.0, color: Colors.teal,),
+          Container(width: 80.0, height: 80.0, color: Colors.pink,),
         ],
       ),
     );
@@ -32,14 +45,34 @@ class MyFlowDelegate extends FlowDelegate{
 
   @override
   void paintChildren(FlowPaintingContext context){
+    var xShift = margin.left;
+    var yShift = margin.top;
+    var curMaxHeight = 0.0;
     for(int i = 0; i < context.childCount; i++){
-      final shift = context.getChildSize(i)
+      //Judge if current row is full
+      var curRightBorder = xShift + context.getChildSize(i)!.width + margin.left + margin.right;
+      //If current row is not full, calculate shifts and paint child
+      if(curRightBorder < context.size.width){
+        context.paintChild(i,
+          transform: Matrix4.translationValues(xShift + margin.left, yShift + margin.top, 0.0),
+        );
+        xShift = curRightBorder;
+        curMaxHeight = max(curMaxHeight, context.getChildSize(i)!.height);
+      }
+      else{
+        xShift = margin.left;
+        yShift = yShift + curMaxHeight + margin.top + margin.bottom;
+        context.paintChild(i,
+          transform: Matrix4.translationValues(xShift + margin.left, yShift + margin.top, 0.0),
+        );
+        xShift = xShift + context.getChildSize(i)!.width + margin.left + margin.right;
+      }
     }
   }
 
   @override
   Size getSize(BoxConstraints constraints){
-    return const Size(double.infinity, 200.0);
+    return const Size(double.infinity, 400.0);
   }
 
   @override
--- chap_4_layout_demo/lib/main.dart
+++ chap_4_layout_demo/lib/main.dart
@@ -1,7 +1,7 @@
 /*
  * @Author       : Linloir
  * @Date         : 2022-02-10 14:44:56
- * @LastEditTime : 2022-02-10 21:22:30
+ * @LastEditTime : 2022-02-13 00:07:19
  * @Description  : 
  */
 import 'package:flutter/material.dart';
@@ -25,6 +25,41 @@ class MyApp extends StatelessWidget {
       initialRoute: "/",
       theme: ThemeData(
         primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Layout Demo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+              child: ElevatedButton(
+                child: Row(
+                  children: const [
+                    Icon(Icons.layers_rounded),
+                    Padding(
+                      padding: EdgeInsets.only(left: 5.0),
+                      child: Text('Flow Layout'),
+                    )
+                  ],
+                ),
+                onPressed: () => Navigator.of(context).pushNamed('/Flow'),
+              )
+            ),
+          ],
+        ),
       ),
     );
   }
